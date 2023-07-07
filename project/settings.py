f0b06d26d5d45ac98d2037264e1b66f3793e6eb8
Tomyyy-1337 <tom.passberg@hotmail.com>
2023-07-08 01:53:39 +0200

    -

--- project/settings.py
+++ project/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django_bootstrap5',
     "Speisekarte.apps.SpeisekarteConfig",
     "Startseite.apps.StartseiteConfig",
 ]
@@ -53,7 +53,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / "templates"
+            BASE_DIR + "/templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
